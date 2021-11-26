a6dcd66ab8adf7aa41e3e2edae00bd11856c836f
Stlukasz <lukasz.stolarczyk@microsoft.wsei.edu.pl>
2021-11-26 19:30:39 +0100

    init

--- Aksoloth/Assets/EnemyAI.cs
+++ Aksoloth/Assets/EnemyAI.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     Animator animator;
-
+    public float EnemyXRotation;
     public Transform Player;
     public float viewRange;
     public float atackRange;
     public float speed;
+     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(Player.position, this.transform.position)< viewRange)
+        
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(EnemyXRotation, eulerRotation.y, 0);
+
+        if (Vector3.Distance(Player.position, this.transform.position)< viewRange)
         {
             Vector3 kieronek = Player.position - this.transform.position;
             kieronek.y = 0;
+
+            
+
 
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(kieronek), 0.1f);
 
@@ -30,16 +39,19 @@ public class EnemyAI : MonoBehaviour
             if (kieronek.magnitude > atackRange)
             {
                 this.transform.Translate(0, 0, speed);
-                animator.SetBool("atack",true);
+                animator.SetBool("atack", false);
             }
             else
             {
-                animator.SetBool("atack", false);
+                animator.SetBool("atack", true);
+                animator.SetBool("run", false);
+
             }
         }
         else
         {
             animator.SetBool("run", false);
+           
         }
     }
 }
